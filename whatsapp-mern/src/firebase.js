cf0d3cf66d341d0fff66dc972ada3ea1693c2174
Harapriya Mohapatra <jhanja2000@gmail.com>
2021-10-20 08:38:17 +0530

    first commit

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBTESp4bl0vSuuI0xenOTWwiizE6Yy_-GE",
+    authDomain: "whatsapp-mern-30633.firebaseapp.com",
+    projectId: "whatsapp-mern-30633",
+    storageBucket: "whatsapp-mern-30633.appspot.com",
+    messagingSenderId: "721996092799",
+    appId: "1:721996092799:web:3d47814ea8161ebef5764a",
+    measurementId: "G-KQVFMY1PHJ"
+  };
